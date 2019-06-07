d2c6b3209c7b7e2f6878a4980142c3140221eb5d
98phil <2215347250@qq.com>
2019-06-07 15:00:34 +0800

    全部清空

--- hello.cpp
+++ hello.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-	cout << "阿肥，我知道怎么解决合并加载慢的问题了" << endl;
-	for (int i = 0; i < 1; i++)
-	{
-		cout << "66" << endl;
-	}
-	return 0;
+	cout << "全部清空" << endl;
 
+
+
+
+
+	return 0;
+	//666
 }
